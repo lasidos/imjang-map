42791489c8931614f2a255edc20d791e0b88eff2
lasidos <zigur82@gmail.com>
2026-06-27 16:11:45 +0900

    Rebrand to 단지노트

--- assets/index-9Kwot1fz.js
+++
@@ -1,2 +0,0 @@
-import{a as i}from"./index-J9gP_iQG.js";const t=i("PushNotifications",{});export{t as PushNotifications};
-//# sourceMappingURL=index-9Kwot1fz.js.map
---
+++ assets/index-Bvxx0s7l.js
@@ -0,0 +1,2 @@
+import{a as i}from"./index-DSEp8eYa.js";const t=i("PushNotifications",{});export{t as PushNotifications};
+//# sourceMappingURL=index-Bvxx0s7l.js.map
