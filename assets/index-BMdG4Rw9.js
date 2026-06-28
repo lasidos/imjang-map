28cd77c2891e90d00d87a34921d6d8566b5a01cd
lasidos <zigur82@gmail.com>
2026-06-28 17:27:06 +0900

    Fix: recenter refreshes complex list + more note editor bottom padding

---
+++ assets/index-BMdG4Rw9.js
@@ -0,0 +1,2 @@
+import{a as i}from"./index-DfC8LSAa.js";const t=i("PushNotifications",{});export{t as PushNotifications};
+//# sourceMappingURL=index-BMdG4Rw9.js.map
--- assets/index-CBHFlim_.js
+++
@@ -1,2 +0,0 @@
-import{a as i}from"./index-3_tAttyA.js";const t=i("PushNotifications",{});export{t as PushNotifications};
-//# sourceMappingURL=index-CBHFlim_.js.map
