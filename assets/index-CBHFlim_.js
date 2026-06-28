3e1b45fe647fa90cbdaafe2a48993ce975ca4520
lasidos <zigur82@gmail.com>
2026-06-28 13:41:05 +0900

    Fix 임장노트 bottom overlap: add scroll padding for fixed footer

---
+++ assets/index-CBHFlim_.js
@@ -0,0 +1,2 @@
+import{a as i}from"./index-3_tAttyA.js";const t=i("PushNotifications",{});export{t as PushNotifications};
+//# sourceMappingURL=index-CBHFlim_.js.map
--- assets/index-mRwaINL3.js
+++
@@ -1,2 +0,0 @@
-import{a as i}from"./index-CmwE5OA8.js";const t=i("PushNotifications",{});export{t as PushNotifications};
-//# sourceMappingURL=index-mRwaINL3.js.map
