4514a6b9f7bbe30f70e6695adf3576feea6da2af
lasidos <zigur82@gmail.com>
2026-06-29 09:48:47 +0900

    fix: 위치권한 온보딩 버튼 '위치 사용 허용'→'계속' (App Store 5.1.1(iv) 거부 대응)

--- assets/index-6k6z6hSq.js
+++
@@ -1,2 +0,0 @@
-import{a as i}from"./index-C-GOvlRe.js";const t=i("PushNotifications",{});export{t as PushNotifications};
-//# sourceMappingURL=index-6k6z6hSq.js.map
---
+++ assets/index-CxWF5s8O.js
@@ -0,0 +1,2 @@
+import{a as i}from"./index-DO21ig3B.js";const t=i("PushNotifications",{});export{t as PushNotifications};
+//# sourceMappingURL=index-CxWF5s8O.js.map
