0cb526d873db722f73cdb3f55740e664c880353b
lasidos <zigur82@gmail.com>
2026-07-01 18:48:04 +0900

    API base를 Cloudflare Tunnel(danjinote-api.tripvoice.co.kr)로 전환

---
+++ assets/index-BeV3xigU.js
@@ -0,0 +1,2 @@
+import{a as i}from"./index-CCmXH1Ji.js";const t=i("PushNotifications",{});export{t as PushNotifications};
+//# sourceMappingURL=index-BeV3xigU.js.map
--- assets/index-Beqxu0KQ.js
+++
@@ -1,2 +0,0 @@
-import{a as i}from"./index-D2BxUu9s.js";const t=i("PushNotifications",{});export{t as PushNotifications};
-//# sourceMappingURL=index-Beqxu0KQ.js.map
