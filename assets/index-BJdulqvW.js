4d569ddcfa8f5106e361ddc70eb713b17e3588e6
lasidos <zigur82@gmail.com>
2026-07-04 20:19:31 +0900

    초대링크 스킴을 등록 확실한 kr.publicad.imjangnote로 교체(주소무효 해결)

---
+++ assets/index-BJdulqvW.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-DzbWFMZf.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- assets/index-DN09PGz7.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-2irnc29f.js";const t=i("PushNotifications",{});export{t as PushNotifications};
