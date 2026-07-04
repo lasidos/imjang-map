e59ec7a608f1ed4a70e8781571f5ebee24637f0c
lasidos <zigur82@gmail.com>
2026-07-04 17:18:46 +0900

    네이티브 안내: 웹 위치 시드로 Live Activity 포그라운드 즉시 시작(잠금 즉시표시)

---
+++ assets/index-DmzT6EV-.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-BknNv37S.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- assets/index-DnOA1cro.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-h3TikaTi.js";const t=i("PushNotifications",{});export{t as PushNotifications};
