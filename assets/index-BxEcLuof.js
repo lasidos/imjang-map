e77354abec1bec5d378be95fea8c547d4fe09389
lasidos <zigur82@gmail.com>
2026-07-06 22:05:05 +0900

    fix: 설정 버전 네이티브 동기화

--- assets/index-BxEcLuof.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-C0K0ZwG1.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-DGXSjgAB.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-CAjxXgaq.js";const t=i("PushNotifications",{});export{t as PushNotifications};
