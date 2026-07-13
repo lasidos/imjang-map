fecee238ff2910b28f189414c9808f51bb8d651e
lasidos <zigur82@gmail.com>
2026-07-13 10:48:25 +0900

    위젯 수명=앱 수명: 콜드런치 잔존 위젯 정리, 해제된 위젯 재생성, widgetOn 네이티브 영속(앱실행=위젯실행)

--- assets/index-PwMjk5uV.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-ClTq_VhL.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-rp0aw795.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-o66WN3Yq.js";const t=i("PushNotifications",{});export{t as PushNotifications};
