ad54c296b579025c2fea08739ff74b28dac3c96e
lasidos <zigur82@gmail.com>
2026-07-17 15:46:13 +0900

    fix(android): 상태바 흰 배경+어두운 아이콘 고정(회색 #757575 제거)

---
+++ assets/index-RAOFOEro.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-g9yU7AsW.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- assets/index-UAg4bYIL.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-4UR69VAW.js";const t=i("PushNotifications",{});export{t as PushNotifications};
