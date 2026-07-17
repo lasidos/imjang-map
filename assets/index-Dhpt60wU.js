35fa48c650fd35f41b8551930c09e22d8b394ee0
lasidos <zigur82@gmail.com>
2026-07-17 16:05:18 +0900

    style: 헤더 상단 여백 축소(로고를 상태바 쪽으로 끌어올림)

--- assets/index-Dhpt60wU.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-B8HxJvcV.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-x4Au7uGt.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-tZoeW_0_.js";const t=i("PushNotifications",{});export{t as PushNotifications};
