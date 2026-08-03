de6980fee333d7d738c665732b2c0563aae03d59
lasidos <zigur82@gmail.com>
2026-08-03 15:15:20 +0900

    네이버 매물 버튼: K-APT '아파트' 접미사 제거(≥5자)로 네이버 등록명과 매칭 개선 (검색 실패 완화)

--- assets/index-BdM95oQy.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-CoSY89T0.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-BiHa-ql-.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-ypps_JYg.js";const t=i("PushNotifications",{});export{t as PushNotifications};
