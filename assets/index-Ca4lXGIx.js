c2c48e81107d1bf0605f369cc02e5a14dfeda24f
lasidos <zigur82@gmail.com>
2026-08-04 13:04:08 +0900

    네이버 매물 버튼 리디자인: 화살표 추가+연한배경 버튼감 강화, 안내문구 2번째 줄

--- assets/index-Ca4lXGIx.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-B1W2xtzF.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-DY3JqnfX.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-Jq9EvW--.js";const t=i("PushNotifications",{});export{t as PushNotifications};
