4972107f5c2d9616bab2ea639be0f89a4e1f6452
lasidos <zigur82@gmail.com>
2026-08-17 12:28:16 +0900

    임장 탭 목업 반영: 기록 상세(요약·평가·댓글)·대댓글·수정·방문시간·방문완료 토글

--- assets/index-BiGYZ2DP.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-CFtU7hvu.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-nzgpdYdp.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-DGRFhhAB.js";const t=i("PushNotifications",{});export{t as PushNotifications};
