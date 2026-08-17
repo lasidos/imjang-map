2186c716864193d31ea51c86bc7b4f6c4bfc4a89
lasidos <zigur82@gmail.com>
2026-08-17 13:04:44 +0900

    알림에서 초대 수락 시 해당 임장으로 이동

--- assets/index-BrRXHN3S.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-D0xUsyKS.js";const t=i("PushNotifications",{});export{t as PushNotifications};
---
+++ assets/index-OCXG52d-.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-B0093_NM.js";const t=i("PushNotifications",{});export{t as PushNotifications};
