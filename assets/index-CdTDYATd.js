5f06fb71aecd100ea93d3ad0c7488b06d2104c08
lasidos <zigur82@gmail.com>
2026-08-27 11:22:35 +0900

    도메인 전환 준비: GHPAGES 빌드 base를 상대경로로 (두 URL 동시 호환)

---
+++ assets/index-CdTDYATd.js
@@ -0,0 +1,1 @@
+import{a as i}from"./index-CQwiO4g3.js";const t=i("PushNotifications",{});export{t as PushNotifications};
--- assets/index-CjDjSFuD.js
+++
@@ -1,1 +0,0 @@
-import{a as i}from"./index-CL-5TSXo.js";const t=i("PushNotifications",{});export{t as PushNotifications};
